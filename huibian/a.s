5feb5445b21b7d8145574f65c9bed5c24616fb0c
ZhouYuJiang-329 <zhouyujiang73@gmail.com>
2026-05-21 09:48:31 +0800

    doc:添加栈的理解,彻底明白rbp,rsp

--- huibian/a.s
+++ huibian/a.s
@@ -1,226 +1,49 @@
 	.file	"a.c"
 	.text
-.Ltext0:
-	.file 0 "/home/zyj/Code/OS/huibian" "a.c"
+
+# add 函数：设置局部变量并返回
 	.globl	add
 	.type	add, @function
 add:
 .LFB0:
-	.file 1 "a.c"
-	.loc 1 2 1
 	.cfi_startproc
-	endbr64
-	pushq	%rbp
+	endbr64				# CET: 启用控制流完整性，防止跳转攻击
+	pushq	%rbp			# 保存调用者的帧指针
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	movq	%rsp, %rbp		# 建立当前函数的栈帧
 	.cfi_def_cfa_register 6
-	.loc 1 3 9
-	movl	$10, -4(%rbp)
-	.loc 1 4 1
-	nop
-	popq	%rbp
+	movl	$10, -4(%rbp)		# 局部变量 a = 10
+	nop				# 空操作（编译器填充对齐）
+	popq	%rbp			# 恢复调用者的帧指针
 	.cfi_def_cfa 7, 8
-	ret
+	ret				# 返回调用者
 	.cfi_endproc
 .LFE0:
 	.size	add, .-add
+
+# main 函数：调用 add 并退出
 	.globl	main
 	.type	main, @function
 main:
 .LFB1:
-	.loc 1 6 11
 	.cfi_startproc
-	endbr64
-	pushq	%rbp
+	endbr64				# CET: 启用控制流完整性
+	pushq	%rbp			# 保存调用者的帧指针
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	movq	%rsp, %rbp		# 建立当前函数的栈帧
 	.cfi_def_cfa_register 6
-	.loc 1 7 5
-	movl	$0, %eax
-	call	add
-	.loc 1 8 12
-	movl	$0, %eax
-	.loc 1 9 1
-	popq	%rbp
+	movl	$0, %eax		# 设置返回值寄存器 eax = 0（准备调用）
+	call	add			# 调用 add 函数
+	movl	$0, %eax		# 设置返回值 eax = 0（return 0）
+	popq	%rbp			# 恢复调用者的帧指针
 	.cfi_def_cfa 7, 8
-	ret
+	ret				# 返回，程序结束
 	.cfi_endproc
 .LFE1:
 	.size	main, .-main
-.Letext0:
-	.section	.debug_info,"",@progbits
-.Ldebug_info0:
-	.long	0x78
-	.value	0x5
-	.byte	0x1
-	.byte	0x8
-	.long	.Ldebug_abbrev0
-	.uleb128 0x1
-	.long	.LASF2
-	.byte	0x1d
-	.long	.LASF0
-	.long	.LASF1
-	.quad	.Ltext0
-	.quad	.Letext0-.Ltext0
-	.long	.Ldebug_line0
-	.uleb128 0x2
-	.long	.LASF3
-	.byte	0x1
-	.byte	0x6
-	.byte	0x5
-	.long	0x4c
-	.quad	.LFB1
-	.quad	.LFE1-.LFB1
-	.uleb128 0x1
-	.byte	0x9c
-	.uleb128 0x3
-	.byte	0x4
-	.byte	0x5
-	.string	"int"
-	.uleb128 0x4
-	.string	"add"
-	.byte	0x1
-	.byte	0x1
-	.byte	0x6
-	.quad	.LFB0
-	.quad	.LFE0-.LFB0
-	.uleb128 0x1
-	.byte	0x9c
-	.uleb128 0x5
-	.string	"a"
-	.byte	0x1
-	.byte	0x3
-	.byte	0x9
-	.long	0x4c
-	.uleb128 0x2
-	.byte	0x91
-	.sleb128 -20
-	.byte	0
-	.byte	0
-	.section	.debug_abbrev,"",@progbits
-.Ldebug_abbrev0:
-	.uleb128 0x1
-	.uleb128 0x11
-	.byte	0x1
-	.uleb128 0x25
-	.uleb128 0xe
-	.uleb128 0x13
-	.uleb128 0xb
-	.uleb128 0x3
-	.uleb128 0x1f
-	.uleb128 0x1b
-	.uleb128 0x1f
-	.uleb128 0x11
-	.uleb128 0x1
-	.uleb128 0x12
-	.uleb128 0x7
-	.uleb128 0x10
-	.uleb128 0x17
-	.byte	0
-	.byte	0
-	.uleb128 0x2
-	.uleb128 0x2e
-	.byte	0
-	.uleb128 0x3f
-	.uleb128 0x19
-	.uleb128 0x3
-	.uleb128 0xe
-	.uleb128 0x3a
-	.uleb128 0xb
-	.uleb128 0x3b
-	.uleb128 0xb
-	.uleb128 0x39
-	.uleb128 0xb
-	.uleb128 0x49
-	.uleb128 0x13
-	.uleb128 0x11
-	.uleb128 0x1
-	.uleb128 0x12
-	.uleb128 0x7
-	.uleb128 0x40
-	.uleb128 0x18
-	.uleb128 0x7c
-	.uleb128 0x19
-	.byte	0
-	.byte	0
-	.uleb128 0x3
-	.uleb128 0x24
-	.byte	0
-	.uleb128 0xb
-	.uleb128 0xb
-	.uleb128 0x3e
-	.uleb128 0xb
-	.uleb128 0x3
-	.uleb128 0x8
-	.byte	0
-	.byte	0
-	.uleb128 0x4
-	.uleb128 0x2e
-	.byte	0x1
-	.uleb128 0x3f
-	.uleb128 0x19
-	.uleb128 0x3
-	.uleb128 0x8
-	.uleb128 0x3a
-	.uleb128 0xb
-	.uleb128 0x3b
-	.uleb128 0xb
-	.uleb128 0x39
-	.uleb128 0xb
-	.uleb128 0x11
-	.uleb128 0x1
-	.uleb128 0x12
-	.uleb128 0x7
-	.uleb128 0x40
-	.uleb128 0x18
-	.uleb128 0x7a
-	.uleb128 0x19
-	.byte	0
-	.byte	0
-	.uleb128 0x5
-	.uleb128 0x34
-	.byte	0
-	.uleb128 0x3
-	.uleb128 0x8
-	.uleb128 0x3a
-	.uleb128 0xb
-	.uleb128 0x3b
-	.uleb128 0xb
-	.uleb128 0x39
-	.uleb128 0xb
-	.uleb128 0x49
-	.uleb128 0x13
-	.uleb128 0x2
-	.uleb128 0x18
-	.byte	0
-	.byte	0
-	.byte	0
-	.section	.debug_aranges,"",@progbits
-	.long	0x2c
-	.value	0x2
-	.long	.Ldebug_info0
-	.byte	0x8
-	.byte	0
-	.value	0
-	.value	0
-	.quad	.Ltext0
-	.quad	.Letext0-.Ltext0
-	.quad	0
-	.quad	0
-	.section	.debug_line,"",@progbits
-.Ldebug_line0:
-	.section	.debug_str,"MS",@progbits,1
-.LASF2:
-	.string	"GNU C17 13.3.0 -mtune=generic -march=x86-64 -g -fasynchronous-unwind-tables -fstack-protector-strong -fstack-clash-protection -fcf-protection"
-.LASF3:
-	.string	"main"
-	.section	.debug_line_str,"MS",@progbits,1
-.LASF0:
-	.string	"a.c"
-.LASF1:
-	.string	"/home/zyj/Code/OS/huibian"
+
 	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04.1) 13.3.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
