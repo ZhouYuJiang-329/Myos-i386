c1e97d79d029fd119720486d6f557a50cfa3b97c
ZhouYuJiang-329 <zhouyujiang73@gmail.com>
2026-05-23 23:34:05 +0800

    refactor：重构 boot/setup 硬盘读取和保护模式切换
    - boot.asm：重写 read_hd 函数，支持多扇区循环读取、等待硬盘就绪
    - setup.asm：简化 A20 启用（端口 92h），GDT 独立 section，内核在保护模式下读取
    - main.c：kernel_main 输出 "Hello OS!" 字符串到 VGA 显存

--- init/main.c
+++ init/main.c
@@ -3,8 +3,14 @@
 //
 
 void kernel_main(void) {
-    int a = 0;
-
     char* video = (char*)0xb8000;
-    *video = 'G';
+      char* msg = "Hello OS!";
+      int i = 0;
+
+      while (msg[i] != '\0') {
+          video[i * 2] = msg[i];      // 字符
+          video[i * 2 + 1] = 0x07;    // 属性（白字黑底）
+          i++;
+      }
+
 }
--- oskernel/boot/boot.asm
+++ oskernel/boot/boot.asm
@@ -1,78 +1,29 @@
-[ORG 0x7c00]
+[ORG  0x7c00]
 
 [SECTION .data]
-BOOT_MAIN_ADDR EQU 0x500
+BOOT_MAIN_ADDR equ 0x500
 
 [SECTION .text]
 [BITS 16]
 global boot_start
 boot_start:
-    ; 清除屏幕
-    mov  ax,3
+    ; 设置屏幕模式为文本模式，清除屏幕
+    mov ax, 3
     int 0x10
-    ; 打印 Hello World
-    mov si, msg
-    call print_string
 
-    call read_hard_disk
-    mov si,read_disking
-    call print_string
-   ; 验证状态
-    ; 3 0表示硬盘未准备好与主机交换数据 1表示准备好了
-    ; 7 0表示硬盘不忙 1表示硬盘忙
-    ; 0 0表示前一条指令正常执行 1表示执行出错 出错信息通过0x1f1端口获得
-.read_check:
-    mov dx, 0x1f7
-    in al, dx
-    and al, 0b10001000  ; 取硬盘状态的第3、7位
-    cmp al, 0b00001000  ; 硬盘数据准备好了且不忙了
-    jnz .read_check
-
-    ; 读数据
-    mov dx, 0x1f0
-    mov cx, 256
-    mov edi, BOOT_MAIN_ADDR
-.read_data:
-    in ax, dx
-    mov [edi], ax
-    add edi, 2
-    loop .read_data
+    ; 将setup读入内存
+    mov edi, BOOT_MAIN_ADDR ; 读到哪里
+    mov ecx, 1      ; 从哪个扇区开始读
+    mov bl, 2       ; 读多少扇区
+    call read_hd
 
     ; 跳过去
-    mov     si, jump_setup
-    call    print_string
+    mov     si, jmp_to_setup
+    call    print
 
-    jmp     BOOT_MAIN_ADDR  
+    jmp     BOOT_MAIN_ADDR
 
-    ; 调转
-    jmp BOOT_MAIN_ADDR
-read_floppy_error:
-    mov     si, read_floppy_error_msg
-    call    print_string
-    jmp $
-
-; 打印字符串函数
-; 输入: DS:SI 指向字符串
-print_string:
-    pusha
-.loop:
-    lodsb           ; 从 DS:SI 加载一个字节到 AL, SI 自增
-    test al, al     ; 检查是否为字符串结束符 (0)
-    jz .done
-    mov ah, 0x0e    ; BIOS 中断 0x10 功能号: 电传打字机输出
-    mov bh, 0x00    ; 页号
-    mov bl, 0x07    ; 颜色属性 (浅灰色)
-    int 0x10        ; 调用 BIOS 中断
-    jmp .loop
-.done:
-    popa
-    ret
-
-read_hard_disk:
-     ; 读硬盘第2扇区到 0x500
-    mov ecx, 1  ; 从硬盘哪个扇区开始读 (LBA 1 = setup.bin)
-    mov bl, 1   ; 读取的扇区数量 (setup.bin < 512 字节，1 扇区足够)
-
+read_hd:
     ; 0x1f2 8bit 指定读取或写入的扇区数
     mov dx, 0x1f2
     mov al, bl
@@ -85,7 +36,7 @@ read_hard_disk:
 
     ; 0x1f4 8bit iba地址的中八位 8-15
     inc dx
-    mov al, ch      ; 取中8位
+    mov al, ch
     out dx, al
 
     ; 0x1f5 8bit iba地址的高八位 16-23
@@ -98,9 +49,12 @@ read_hard_disk:
     ; 0-3 位iba地址的24-27
     ; 4 0表示主盘 1表示从盘
     ; 5、7位固定为1
-    ; 6 0表示CHS模式，1表示LBA模式
+    ; 6 0表示CHS模式，1表示LAB模式
     inc dx
-    mov al, 0xE0        ; LBA 模式，主盘
+    shr ecx, 8
+    and cl, 0b1111
+    mov al, 0b1110_0000     ; LBA模式
+    or al, cl
     out dx, al
 
     ; 0x1f7 8bit  命令或状态端口
@@ -108,18 +62,67 @@ read_hard_disk:
     mov al, 0x20
     out dx, al
 
+    ; 设置loop次数，读多少个扇区要loop多少次
+    mov cl, bl
+.start_read:
+    push cx     ; 保存loop次数，防止被下面的代码修改破坏
+
+    call .wait_hd_prepare
+    call read_hd_data
+
+    pop cx      ; 恢复loop次数
+
+    loop .start_read
+
+.return:
     ret
 
-msg:
-    db "Hello World boot!",10, 13, 0
-jump_setup:
-    db "jmp setup!", 10, 13,0
-read_floppy_error_msg:
-    db "read floppy error!", 10, 13, 0
-read_disking:
-    db "read disk ing...",13,10,0
-; 填充到 510 字节
-times 510-($-$$) db 0
+; 一直等待，直到硬盘的状态是：不繁忙，数据已准备好
+; 即第7位为0，第3位为1，第0位为0
+.wait_hd_prepare:
+    mov dx, 0x1f7
 
-; 引导扇区签名
-dw 0xaa55
+.check:
+    in al, dx
+    and al, 0b1000_1000
+    cmp al, 0b0000_1000
+    jnz .check
+
+    ret
+
+; 读硬盘，一次读两个字节，读256次，刚好读一个扇区
+read_hd_data:
+    mov dx, 0x1f0
+    mov cx, 256
+
+.read_word:
+    in ax, dx
+    mov [edi], ax
+    add edi, 2
+    loop .read_word
+
+    ret
+
+; 如何调用
+; mov     si, msg   ; 1 传入字符串
+; call    print     ; 2 调用
+print:
+    mov ah, 0x0e
+    mov bh, 0
+    mov bl, 0x01
+.loop:
+    mov al, [si]
+    cmp al, 0
+    jz .done
+    int 0x10
+
+    inc si
+    jmp .loop
+.done:
+    ret
+
+jmp_to_setup:
+    db "jump to setup...", 10, 13, 0
+
+times 510 - ($ - $$) db 0
+db 0x55, 0xaa
